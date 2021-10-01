9e7dd13d51048b95418e3448d6f45da6d0478902
Stiven <stivencastillo.90@gmail.com>
2021-10-01 14:33:58 -0500

    Create container deploy script

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -1,3 +1,7 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const packageJson = require('../package.json');
+
 module.exports = {
   module: {
     rules: [
@@ -13,5 +17,18 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ]
 }
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,8 +1,5 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
-const packageJson = require('../package.json')
 
 const devConfig = {
   mode: "development",
@@ -12,19 +9,6 @@ const devConfig = {
       index: "index.html",
     },
   },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'marketing',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './MarketingApp': './src/bootstrap'
-      },
-      shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
 };
 
 module.exports = merge(commonConfig, devConfig);
